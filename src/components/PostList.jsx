13962c5850fa90b3ddb8d12af6c206c81810701c
Craig Clayton <craig__clayton@outlook.com>
2025-02-26 21:35:39 +0000

    add link to let user go on profile

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
-import { Pagination } from "./Pagination";
-import { Vote } from "./Vote";
-import { db } from "@/db";
-import { POSTS_PER_PAGE } from "@/config";
+import Link from 'next/link';
+import { Pagination } from './Pagination';
+import { Vote } from './Vote';
+import { db } from '@/db';
+import { POSTS_PER_PAGE } from '@/config';
 
 export async function PostList({ currentPage = 1 }) {
   const { rows: posts } =
@@ -18,21 +18,26 @@ export async function PostList({ currentPage = 1 }) {
 
   return (
     <>
-      <ul className="max-w-screen-lg mx-auto p-4 mb-4">
+      <ul className='max-w-screen-lg mx-auto p-4 mb-4'>
         {posts.map((post) => (
           <li
             key={post.id}
-            className=" py-4 flex space-x-6 hover:bg-zinc-200 rounded-lg"
+            className=' py-4 flex space-x-6 hover:bg-zinc-200 rounded-lg'
           >
             <Vote postId={post.id} votes={post.vote_total} />
             <div>
               <Link
                 href={`/post/${post.id}`}
-                className="text-3xl hover:text-pink-500"
+                className='text-3xl hover:text-pink-500'
               >
                 {post.title}
               </Link>
-              <p className="text-zinc-700">posted by {post.name}</p>
+              <p className='text-zinc-700'>
+                posted by{' '}
+                <Link href={`/profile`} className='hover:underline'>
+                  {post.name}
+                </Link>
+              </p>
             </div>
           </li>
         ))}
