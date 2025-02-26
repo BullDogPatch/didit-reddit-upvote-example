8c0df21223852fdff22619d68e950976dc2673e5
Craig Clayton <craig__clayton@outlook.com>
2025-02-26 21:41:23 +0000

    .

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,6 +1,6 @@
-import auth from "../app/middleware";
-import { LoginButton } from "./LoginButton";
-import { LogoutButton } from "./LogoutButton";
+import auth from '../app/middleware';
+import { LoginButton } from './LoginButton';
+import { LogoutButton } from './LogoutButton';
 
 export async function UserInfo() {
   const session = await auth();
@@ -9,13 +9,13 @@ export async function UserInfo() {
     <div>
       {session ? (
         <div>
-          {session.user.name}{" "}
-          <span className="text-xs text-zinc-400 mr-3">#{session.user.id}</span>
+          {session.user.name}{' '}
+          <span className='text-xs text-zinc-400 mr-3'>#{session.user.id}</span>
           <LogoutButton />
         </div>
       ) : (
         <div>
-          <span className="mr-4">Welcome, Guest!</span>
+          <span className='mr-4'>Welcome, Guest!</span>
           <LoginButton />
         </div>
       )}
